75728aa9af0125b35e47d3ffb729192e29683f61
tokonoko12 <mithubackgo@outlook.in>
2025-12-26 23:00:23 +0530

    change icons

--- src/lib/components/ui/VideoPlayer.svelte
+++ src/lib/components/ui/VideoPlayer.svelte
@@ -535,14 +535,14 @@
 										xmlns="http://www.w3.org/2000/svg"
 										fill="none"
 										viewBox="0 0 24 24"
-										stroke-width="1"
+										stroke-width="1.5"
 										stroke="currentColor"
-										class="h-12 w-12 transition-transform group-active:-translate-x-1"
+										class="h-10 w-10 transition-transform group-active:-translate-x-1"
 									>
 										<path
 											stroke-linecap="round"
 											stroke-linejoin="round"
-											d="M15.75 19.5L8.25 12l7.5-7.5"
+											d="M9 15L3 9m0 0l6-6M3 9h12a6 6 0 010 12h-3"
 										/>
 									</svg>
 									<span class="font-mono text-[10px] font-bold tracking-widest uppercase">-15s</span
@@ -619,14 +619,14 @@
 										xmlns="http://www.w3.org/2000/svg"
 										fill="none"
 										viewBox="0 0 24 24"
-										stroke-width="1"
+										stroke-width="1.5"
 										stroke="currentColor"
-										class="h-12 w-12 transition-transform group-active:translate-x-1"
+										class="h-10 w-10 transition-transform group-active:translate-x-1"
 									>
 										<path
 											stroke-linecap="round"
 											stroke-linejoin="round"
-											d="M8.25 4.5l7.5 7.5-7.5 7.5"
+											d="M15 15l6-6m0 0l-6-6m6 6h-12a6 6 0 000 12h3"
 										/>
 									</svg>
 									<span class="font-mono text-[10px] font-bold tracking-widest uppercase">+15s</span
